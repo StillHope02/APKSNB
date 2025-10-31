c574231b6ff25ca0abbb60ff3eaca0c6179272e7
atique-code <atiquekalwar123@gmail.com>
2025-10-31 18:36:55 +0500

    name update

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
       <Row className="mt-3">
         <Col>
           <a
-            href="/app-release(1).apk"
+            href="/Apk.apk"
             download
             style={{ textDecoration: 'none' }}
             onClick={(e) => {
